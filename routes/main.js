9f48e9c6a66a70d4bd80dfdcd2cb5c58939594bf
dcumm002 <dcumm002@gold.ac.uk>
2023-12-14 18:54:14 +0000

    error checking for login

--- routes/main.js
+++ routes/main.js
@@ -1,111 +1,115 @@
 module.exports = function(app, gymData) {
-    const { check, validationResult } = require ('express-validator');
-    const bcrypt = require('bcrypt');
-    const redirectLogin = (req, res, next) => {
-      if (!req.session.memberID) {
-        res.redirect('./login')
-      } else { next (); }
-    }
+  const redirectLogin = (req, res, next) => {
+    if (!req.session.memberID) {
+      res.redirect('./login')
+    } else { next (); }
+  }
+  const { check, validationResult } = require ('express-validator');
+  const bcrypt = require('bcrypt');
+    
 
-    //Handle our routes
-    app.get('/',function(req,res){
-        res.render('index.ejs', gymData)
-    });
+  //Handle our routes
+  app.get('/',function(req,res){
+      res.render('index.ejs', gymData)
+  });
 
-    app.get('/about',function(req,res){
-        res.render('about.ejs', gymData);
-    });
+  app.get('/about',function(req,res){
+      res.render('about.ejs', gymData);
+  });
 
-    app.get('/register', function (req,res) {
-      res.render('register.ejs', gymData);                                                                     
-    }); 
+  app.get('/register', function (req,res) {
+    res.render('register.ejs', gymData);                                                                     
+  }); 
 
-    app.post('/registered', [
-      check('email').isEmail(),
-      check('password').isLength({ min: 8 })
-    ], function (req,res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          console.log("Validation Errors:", errors.array());
-          res.redirect('./register'); }
-        else {
-          const saltRounds = 10;
-          const plainPassword = req.body.password;
-          bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword){
-          //saving username and password in database
-            if (err) {
-              return console.error(err.message);
-            }
-          let newrecord = [req.sanitize(req.body.first), req.sanitize(req.body.last), req.sanitize(req.body.email), req.sanitize(hashedPassword)];
-          // execute sql query
-          let sqlquery = "INSERT INTO members (firstname, lastname, `email`, `hashedPassword`) VALUES (?,?,?,?)";
-          db.query(sqlquery, newrecord, (err, result) => {
-          //if it fails
-            if (err) {
-              return console.error(err.message);
-            }          
-            result = 'Hello ' + req.body.first + ' ' + req.body.last + ' you are now registered! We will send a confirmation email to you at ' + req.body.email;
-            result += ' Your password is ' + plainPassword + ' and your hashed password is ' + hashedPassword + '<a href='+'./'+'>Home</a>';
-            res.send(result);
-            });
-          })    
-        }                                                        
-    });
+  app.post('/registered', [
+    check('email').isEmail(),
+    check('password').isLength({ min: 8 })
+  ], function (req,res) {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log("Validation Errors:", errors.array());
+        res.redirect('./register'); }
+      else {
+        const saltRounds = 10;
+        const plainPassword = req.body.password;
+        bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword){
+        //saving username and password in database
+          if (err) {
+            return console.error(err.message);
+          }
+        let newrecord = [req.sanitize(req.body.first), req.sanitize(req.body.last), req.sanitize(req.body.email), req.sanitize(hashedPassword)];
+        // execute sql query
+        let sqlquery = "INSERT INTO members (firstname, lastname, `email`, `hashedPassword`) VALUES (?,?,?,?)";
+        db.query(sqlquery, newrecord, (err, result) => {
+        //if it fails
+          if (err) {
+            return console.error(err.message);
+          }          
+          result = 'Hello ' + req.body.first + ' ' + req.body.last + ' you are now registered! We will send a confirmation email to you at ' + req.body.email;
+          result += ' Your password is ' + plainPassword + ' and your hashed password is ' + hashedPassword + '<a href='+'./'+'>Home</a>';
+          res.send(result);
+          });
+        })    
+      }                                                        
+  });
 
-    app.get('/login', function (req,res) {
-      res.render('login.ejs', gymData);                                                                     
-    }); 
+  app.get('/login', function (req,res) {
+    res.render('login.ejs', gymData);                                                                     
+  }); 
 
-    app.post('/loggedin', function (req,res) {
-      let newrecord = [req.sanitize(req.body.name)]
-      let sqlquery = "SELECT hashedPassword FROM members WHERE firstname = ?"      
-      db.query(sqlquery, newrecord, (err, result) => {
-        if(err) {
-          return console.error(err.message);
-        }
-        else {
-          console.error("logged in")
-
-          hashedPassword = result[0].hashedPassword;
-          bcrypt.compare(req.body.password, hashedPassword, function(err, result) {
-            if(err) {
-              return console.error(err.message);
-            }
-            else if (result == true) {
-              //save user session here, when login is successful
-              req.session.memberID = req.body.first;
-              //res.send('You are now logged in! <a href='+'./'+'>Home</a>');
-              res.send('Welcome ' + req.body.name + '. You are now logged in! <a href='+'./'+'>Home</a>');
-            }
-            else {
-              res.send('You have entered an incorrect input, please try again');
-            }
+  app.post('/loggedin', function (req,res) {
+    let newrecord = [req.sanitize(req.body.name)]
+    let sqlquery = "SELECT hashedPassword FROM members WHERE firstname = ?";
+    db.query(sqlquery, newrecord, (err, result) => {
+      if (err) {
+        console.error('Database error:', err.message);
+        res.status(500).send('Internal server error. Please try again later. <a href='+'./'+'>Home</a>');
+      } else {
+        if (!result || !result[0] || !result[0].hashedPassword) {
+          console.error('No user found or incorrect query result.');
+          res.status(401).send('That name is not recognised, please try again. <a href='+'./login'+'>Return</a>');
+        } else {
+            const hashedPassword = result[0].hashedPassword;
+            bcrypt.compare(req.body.password, hashedPassword, function (err, result) {
+              if (err) {
+                console.error('bcrypt error:', err.message);
+                res.status(500).send('Internal server error. Please try again later.<a href='+'./'+'>Home</a>');
+              } else if (result === true) {
+                //save user session here, when login is successful
+                  req.session.memberID = req.body.first;
+                  console.error("logged in")
+                  res.send('Welcome ' + req.body.name + '. You are now logged in! <a href='+'./'+'>Home</a>');
+              } else {
+                console.error('Incorrect password');
+                res.status(401).send('You have entered an incorrect password, please try again. <a href='+'./login'+'>Return</a>');
+              }
           });
         }
-      });
+      }
     });
+  });
 
-    app.get('/logout',  (req,res) => {  //redirectLogin, for some reason this prevents the user from logging out
-      req.session.destroy(err => {
+  app.get('/logout',  (req,res) => {  //I can't include redirectLogin, for some reason this prevents the user from logging out
+    req.session.destroy(err => {
+      if (err) {
+        return res.redirect('./')
+      }
+      console.error("logged out")
+      res.send('You are now logged out. <a href='+'./'+'>Home</a>');
+    });
+  });
+
+  app.get('/classes', function(req, res) {
+    let sqlquery = "SELECT * FROM classes"; // query database to get all the classes available
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
         if (err) {
-          return res.redirect('./')
+            res.redirect('./'); 
         }
-        console.error("logged out")
-        res.send('You are now logged out. <a href='+'./'+'>Home</a>');
-      });
+        let newData = Object.assign({}, gymData, {availableClasses:result});
+        console.log(newData)
+        res.render("classes.ejs", newData)
     });
-
-    app.get('/classes', function(req, res) {
-      let sqlquery = "SELECT * FROM classes"; // query database to get all the classes available
-      // execute sql query
-      db.query(sqlquery, (err, result) => {
-          if (err) {
-              res.redirect('./'); 
-          }
-          let newData = Object.assign({}, gymData, {availableClasses:result});
-          console.log(newData)
-          res.render("classes.ejs", newData)
-      });
   });
 
   app.get('/search',function(req,res){
